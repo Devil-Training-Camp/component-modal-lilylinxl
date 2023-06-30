ae3f823d44d93927e90bc74cfb88ca2a7b4ff60b
linxl <lin1203@foxmail.com>
2023-06-30 19:59:35 +0800

    chore :swc模式移除

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:true
+  server: {
+    host: true,
   },
   css: {
     preprocessorOptions: {
@@ -14,4 +14,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
